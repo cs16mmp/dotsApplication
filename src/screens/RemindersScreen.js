d9cd74f4a9c0dc3cd05307abf6892ab7f45a8caf
Mario Palmis <1522680@brunel.ac.uk>
2020-03-30 21:19:21 +0100

    Add screen layout to the remidners Screen

--- src/screens/RemindersScreen.js
+++ src/screens/RemindersScreen.js
@@ -2,15 +2,16 @@ import * as React from 'react'
 import { View } from 'react-native'
 
 import RemindersLayout from '../layouts/RemindersLayout'
-
+import ScreenLayout from '../layouts/ScreenLayout'
 
 export default class RemindersScreen extends React.Component {
 
     render() {
         return (
-            <RemindersLayout>
-
-            </RemindersLayout>
+            <ScreenLayout
+                body={RemindersLayout()}
+                title="Reminders">
+            </ScreenLayout>
         )
     }
 }
