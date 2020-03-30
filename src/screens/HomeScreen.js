f938fd5a5336bc73ac34d0a0cd77c90977666610
Mario Palmis <1522680@brunel.ac.uk>
2020-03-30 21:19:44 +0100

    Add the Screen Layout to the Home Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,40 @@
 import * as React from 'react'
-import * as RootNavigation from '../navigation/RootNavigator'
 
-import HomeLayout from '../layouts/HomeLayout'
+import HomeItemLayout from '../layouts/HomeItemLayout'
+import ScreenLayout from '../layouts/ScreenLayout'
 
+import * as Components from '../components/Components'
 
+import DATA from '../assets/data/MenuData.json'
 
+function Item(item) {
+    return (
+        <HomeItemLayout
+            textTitle={item.title}
+            textOverlay={item.overlay}
+            textDescription={item.description}
+            iconName={item.iconName}
+            screenID={item.id}
+        />
+    )
+}
+function List(DATA) {
+    return (
+        <Components.FlatListComponent
+            DATA={DATA.Items}
+            itemLayout={({ item }) => Item(item)}
+            title="Home"
+        />
+    )
+}
 export default class HomeScreen extends React.Component {
 
     render() {
         return (
-            <HomeLayout></HomeLayout>
+            <ScreenLayout
+                body={List(DATA)}
+                title="Döts">
+            </ScreenLayout>
         )
     }
 
