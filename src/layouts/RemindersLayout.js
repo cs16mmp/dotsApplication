ab2f345169f0a15119fbbc4319d04e721bb4baa1
Mario Palmis <1522680@brunel.ac.uk>
2020-03-30 21:15:27 +0100

    re-engineer the remidners layout

--- src/layouts/RemindersLayout.js
+++ src/layouts/RemindersLayout.js
@@ -1,31 +1,27 @@
 import * as React from 'react'
-import { View, ScrollView, Text } from 'react-native'
+import { View, ScrollView, Text, Switch } from 'react-native'
 
 import * as Styles from '../styles/Styles'
 import * as Components from '../components/Components'
 
-
-
+function PillboxComponent() {
+    return (
+        <View style={Styles.card('auto', 200, "column")}>
+            <View style={{ flex: 1, maxHeight: 30, flexDirection: 'row', justifyContent: 'space-between', marginBottom: 16}}>
+                <Text style={Styles.text.headline5}>Pillbox</Text>
+                <Switch></Switch>
+            </View>
+            <View style={{ flex: 1, backgroundColor: 'blue' }}>
+            </View>
+        </View>
+    )
+}
 
 export default function RemindersLayout() {
     return (
-
-        <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <View style={{ flex: 1, backgroundColor: 'gray' }}>
             <ScrollView style={{ paddingHorizontal: 16 }}>
-                <View style={{ flex: 1, height: 60, justifyContent: 'flex-end' }}>
-                    <Text style={Styles.text.headline4}>Reminders</Text>
-                </View>
-                <View style={{ flex: 1, height: 24 }} />
-                <View style={Styles.card('auto', 300)}>
-
-                </View>
-                <View style={{ flex: 1, height: 60, justifyContent: 'flex-end' }}>
-                    <Text style={Styles.text.headline4}>Notifications</Text>
-                </View>
-                <View style={{ flex: 1, height: 24 }} />
-                <View style={Styles.card('auto', 300)}>
-
-                </View>
+                <PillboxComponent></PillboxComponent>
             </ScrollView>
         </View>
     )
