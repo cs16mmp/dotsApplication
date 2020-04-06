bf13a464e9ff6d78f820e2aae016342c0d8ea44f
Mario Palmis <1522680@brunel.ac.uk>
2020-04-06 16:14:02 +0100

    Deleted Provider for REDUX

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  View,
-  StatusBar,
 } from 'react-native';
 import NavigationStack from './src/navigation/NavigationStack'
 
+import { store } from './src/navigation/store'
+
+import { Provider } from 'react-redux'
+
+//console.log(store.getState())
+
 const App: () => React$Node = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <NavigationStack></NavigationStack>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationStack></NavigationStack>
+      </SafeAreaView>
+    </Provider>
   );
 };
 
